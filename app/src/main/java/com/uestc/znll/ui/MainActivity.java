b66903ad54d77ea49f961e405c43483147321b30
QaoA <1358730414@qq.com>
2015-10-31 22:55:22 +0800

    mainAcitivity almost completed

--- app/src/main/java/com/uestc/znll/ui/MainActivity.java
+++ app/src/main/java/com/uestc/znll/ui/MainActivity.java
@@ -1,12 +1,17 @@
 package com.uestc.znll.ui;
 
 import android.os.Bundle;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.uestc.znll.R;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class MainActivity extends BaseActivity {
 
@@ -18,7 +23,7 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findWidgets();
-
+        setMainListView();
     }
 
     @Override
@@ -43,6 +48,23 @@ public class MainActivity extends BaseActivity {
 
     private void findWidgets(){
         listView = (ListView)findViewById(R.id.Main_List);
+    }
+    private void setMainListView()
+    {
+        MainListViewAdapter listAdapter = new MainListViewAdapter(this,getData());
+        listView.setAdapter(listAdapter);
+    }
+    private List<Map<String,Object>> getData()
+    {
+        List<Map<String,Object>> list = new ArrayList<Map<String,Object>>();
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("progressvalue", 50);
+        map.put("datatype","闲时流量");
+        map.put("datausage","每日23：00到次日6：00");//"每日23：00到次日6：00"
+        map.put("remainpecent","50%");
+        map.put("remaindetail", "剩余1GB");
+        list.add(map);
+        return list;
     }
 
 }
---
+++ app/src/main/java/com/uestc/znll/ui/MainListViewAdapter.java
@@ -0,0 +1,124 @@
+package com.uestc.znll.ui;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ProgressBar;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import com.uestc.znll.R;
+
+import java.util.List;
+import java.util.Map;
+import java.util.zip.Inflater;
+
+/**
+ * Created by Administrator on 2015/10/31.
+ */
+public class MainListViewAdapter extends BaseAdapter {
+    private Context m_context;
+    private List<Map<String,Object>> m_list;
+    private ProgressBar m_progressbar;
+    private TextView TextViewDataType;
+    private TextView TextViewDataUsage;
+    private TextView TextViewRemainPecent;
+    private TextView TextViewRemainDetail;
+
+
+    public MainListViewAdapter(Context c, List<Map<String,Object>> list){
+        this.m_context= c;
+        this.m_list=list;
+    }
+
+    @Override
+    public int getCount() {
+        return m_list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return m_list.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if(convertView == null) {
+            convertView = LayoutInflater.from(m_context).inflate(R.layout.main_listview_item, null);
+        }
+        m_progressbar = (ProgressBar)convertView.findViewById(R.id.main_listview_progressbar);
+        TextViewDataType = (TextView)convertView.findViewById(R.id.main_listview_textview_datatype);
+        TextViewDataUsage = (TextView)convertView.findViewById(R.id.main_listview_textview_datausage);
+        TextViewRemainPecent = (TextView)convertView.findViewById(R.id.main_listview_textview_remainpercent);
+        TextViewRemainDetail = (TextView)convertView.findViewById(R.id.main_listview_textview_remaindetail);
+
+        Map tmpMap = m_list.get(position);
+        setM_progressbar((Integer)tmpMap.get("progressvalue"));
+        TextViewDataType.setText((String) tmpMap.get("datatype"));
+        TextViewDataUsage.setText((String) tmpMap.get("datausage"));
+        TextViewRemainPecent.setText((String)tmpMap.get("remainpecent"));
+        TextViewRemainDetail.setText((String)tmpMap.get("remaindetail"));
+
+        System.out.println("datatype  " + (String) tmpMap.get("datatype"));
+        System.out.println("datausage  "+(String) tmpMap.get("datausage"));
+        System.out.println("remainpecent  "+(String)tmpMap.get("remainpecent"));
+        System.out.println("remaindetail  "+(String)tmpMap.get("remaindetail"));
+
+        return convertView;
+    }
+
+    private void setM_progressbar(int progress)
+    {
+        if(progress >100)
+        {
+            progress = 100;
+        }
+        if(progress <0)
+        {
+            progress = 0;
+        }
+        m_progressbar.setProgress(progress);
+       switch (progress/10)
+       {
+           case 0:
+                m_progressbar.setProgressDrawable(m_context.getResources().getDrawable(R.drawable.progressbar_color_0_to_10));
+               break;
+           case 1:
+               m_progressbar.setProgressDrawable(m_context.getResources().getDrawable(R.drawable.progressbar_color_10_to_20));
+               break;
+           case 2:
+               m_progressbar.setProgressDrawable(m_context.getResources().getDrawable(R.drawable.progressbar_color_20_to_30));
+               break;
+           case 3:
+               m_progressbar.setProgressDrawable(m_context.getResources().getDrawable(R.drawable.progressbar_color_30_to_40));
+               break;
+           case 4:
+               m_progressbar.setProgressDrawable(m_context.getResources().getDrawable(R.drawable.progressbar_color_40_to_50));
+               break;
+           case 5:
+               m_progressbar.setProgressDrawable(m_context.getResources().getDrawable(R.drawable.progressbar_color_50_to_60));
+               break;
+           case 6:
+               m_progressbar.setProgressDrawable(m_context.getResources().getDrawable(R.drawable.progressbar_color_60_to_70));
+               break;
+           case 7:
+               m_progressbar.setProgressDrawable(m_context.getResources().getDrawable(R.drawable.progressbar_color_70_to_80));
+               break;
+           case 8:
+               m_progressbar.setProgressDrawable(m_context.getResources().getDrawable(R.drawable.progressbar_color_80_to_90));
+               break;
+           case 9:
+           case 10:
+               m_progressbar.setProgressDrawable(m_context.getResources().getDrawable(R.drawable.progressbar_color_90_to_100));
+               break;
+       }
+    }
+}
