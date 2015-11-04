8476f0e85ea90371c51fdb3d98caddbe67c1d3f0
chao <chao9420@gmail.com>
2015-11-04 22:16:56 +0800

    add receiver

---
+++ app/src/main/java/com/uestc/znll/constant/Config.java
@@ -0,0 +1,9 @@
+package com.uestc.znll.constant;
+
+/**
+ * Created by chao on 2015/11/4.
+ */
+public class Config {
+    //从系统获取流量的间隔时间
+    public static final long SYNC_INTERVAL = 2000;//5*60*1000=5分钟
+}
